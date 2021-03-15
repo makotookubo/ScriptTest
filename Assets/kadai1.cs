8a63fb920d835e46942e5a34b90fb6e879420f1e
makotookubo <79546775+makotookubo@users.noreply.github.com>
2021-03-15 22:47:40 +0900

    Initial commit

---
+++ Assets/kadai1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kadai1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] arrays = { 5, 10, 15, 20, 25 };
+
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(arrays[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/kadai3.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    private int hp = 100;
+    private int power = 25;
+    private int mp = 53;
+
+
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+
+        this.hp -= damage;
+    }
+
+    public void Magic()
+    {
+
+        if (mp >= 5)
+        {
+            int rest = mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + rest + "。");
+        }
+
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+
+
+
+}
+
+
+
+public class kadai3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Boss lastboss = new Boss();
+
+        lastboss.Attack();
+        lastboss.Defence(3);
+
+        for (int i = 0; i <= 10; i++)
+        {
+            lastboss.Magic();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
